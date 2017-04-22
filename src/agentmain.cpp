e9dcaa969f0ec47d08462f46a69bfc05a56e52dd
akashche <akashche@redhat.com>
2017-04-22 21:22:56 +0100

    use staticlibs jvmti utils

--- src/agentmain.cpp
+++ src/agentmain.cpp
@@ -5,145 +5,20 @@
  * Created on April 12, 2017, 4:29 PM
  */
 
-#include <jni.h>
-#include <jvmti.h>
-
-#include <cstring>
-#include <atomic>
-#include <chrono>
-#include <functional>
 #include <iostream>
-#include <memory>
-#include <mutex>
 #include <string>
-#include <thread>
 
 #include "staticlib/config.hpp"
-#include "staticlib/concurrent.hpp"
 #include "staticlib/io.hpp"
 #include "staticlib/jni.hpp"
+#include "staticlib/jvmti.hpp"
 #include "staticlib/support.hpp"
 #include "staticlib/tinydir.hpp"
 
-#include "jni_exception.hpp"
-#include "jvmti_exception.hpp"
-
-namespace jvmti_helper {
-
-class jvmti_error_checker {
-public:
-    void operator=(jvmtiError err) {
-        if (JVMTI_ERROR_NONE != err) {
-            throw jvmti_exception(TRACEMSG("JVMTI error code: [" + sl::support::to_string(err) + "]"));
-        }
-    }
-};
-
-class jvmti_env_ptr {
-    std::unique_ptr<jvmtiEnv, std::function<void(jvmtiEnv*)>> jvmti;
-    
-public:
-    jvmti_env_ptr(JavaVM* jvm) :
-    jvmti([jvm] {
-        sl::jni::error_checker ec;
-        jvmtiEnv* env;
-        ec = jvm->GetEnv(reinterpret_cast<void**> (std::addressof(env)), JVMTI_VERSION);
-        return std::unique_ptr<jvmtiEnv, std::function<void(jvmtiEnv*)>>(env, [jvm](jvmtiEnv * ptr) {
-            // pass captured jvm pointer in case of destruction before global jvm pointer init
-            auto scoped_jni = sl::jni::thread_local_jni_env_ptr(jvm);
-            // lets crash early on error
-            jvmti_error_checker ec;
-            ec = ptr->DisposeEnvironment();
-        });
-    }()) { }
-
-    jvmti_env_ptr (const jvmti_env_ptr&) = delete;
-    
-    jvmti_env_ptr& operator=(const jvmti_env_ptr&) = delete;
-    
-    jvmti_env_ptr(jvmti_env_ptr&& other) :
-    jvmti(std::move(other.jvmti)) { }
-    
-    jvmti_env_ptr& operator=(jvmti_env_ptr&& other) {
-        jvmti = std::move(other.jvmti);
-        return *this;
-    }
-    
-    jvmtiEnv* operator->() {
-        return jvmti.get();
-    }
-
-    jvmtiEnv* get() {
-        return jvmti.get();
-    }
-};
-
-
-template<typename T> class jvmti_base {
-protected:
-    jvmti_env_ptr jvmti;
-    std::string options;
-    std::thread worker;
-    
-    jvmti_base(JavaVM* javavm, char* options) :
-    jvmti(javavm),
-    options(nullptr != options ? options : "") {        
-        register_vminit_callback();
-        apply_capabilities();
-        // start worker
-        this->worker = std::thread([this, javavm] {
-            // init global jvm pointer
-            sl::jni::static_java_vm(javavm);
-            // wait for init
-            sl::jni::static_java_vm().await_init_complete();
-            // call inheritor
-            static_cast<T*> (this)->operator()();
-        });
-    }
-    
-    jvmti_base(const jvmti_base&) = delete;
-    
-    void operator=(const jvmti_base&) = delete;
-    
-    // virtual is unnecessary
-    ~jvmti_base() STATICLIB_NOEXCEPT {
-        std::cout << "Shutting down ..." << std::endl;
-        sl::jni::static_java_vm().notify_shutdown();
-        worker.join();
-    }
-        
-    std::unique_ptr<jvmtiCapabilities> capabilities() {
-        auto caps = sl::support::make_unique<jvmtiCapabilities>();
-        std::memset(caps.get(), 0, sizeof(*caps));
-        return caps;
-    }
-    
-private:
-    void apply_capabilities() {
-        auto caps = static_cast<T*> (this)->capabilities();
-        jvmti_error_checker ec;
-        ec = jvmti->AddCapabilities(caps.get());
-    }
-    
-    void register_vminit_callback() {
-        jvmtiEventCallbacks cbs;
-        memset(std::addressof(cbs), 0, sizeof (cbs));
-        cbs.VMInit = [](jvmtiEnv*, JNIEnv*, jthread) {
-            sl::jni::static_java_vm().notify_init_complete();
-        };
-        jvmti_error_checker ec;
-        ec = jvmti->SetEventCallbacks(std::addressof(cbs), sizeof (cbs));
-        ec = jvmti->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, nullptr);
-    }
-};
-
-} // namespace
-
-
-class meminfo : public jvmti_helper::jvmti_base<meminfo> {
+class meminfo : public sl::jvmti::agent_base<meminfo> {
 public:
     meminfo(JavaVM* jvm, char* options) :
-    jvmti_helper::jvmti_base<meminfo>(jvm, options) { }
+    sl::jvmti::agent_base<meminfo>(jvm, options) { }
     
     void operator()() STATICLIB_NOEXCEPT {
         try {
@@ -168,10 +43,10 @@ private:
         std::cout << res << std::endl;
         std::string path = "/proc/self/status";
         auto src = sl::tinydir::file_source(path);
-//        auto sink = sl::io::streambuf_sink(std::cout.rdbuf());
-        auto sink = sl::io::null_sink();
+        auto sink = sl::io::streambuf_sink(std::cout.rdbuf());
         sl::io::copy_all(src, sink);
-//        std::cout << std::endl << std::endl;
+        std::cout << std::endl << std::endl;
+        std::cout << res << std::endl;
     }
 
 };
@@ -190,4 +65,5 @@ JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* jvm, char* options, void* /* reserve
 
 JNIEXPORT void JNICALL Agent_OnUnload(JavaVM* /* vm */) {
     delete global_mi;
+    std::cout << "Shutdown complete" << std::endl;
 }
