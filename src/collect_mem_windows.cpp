b82eb6d6445c21baa185438dc798833eda43aa46
akashche <akashche@redhat.com>
2017-05-02 16:05:43 -0700

    windows mem details

--- src/collect_mem_windows.cpp
+++ src/collect_mem_windows.cpp
@@ -10,16 +10,33 @@
 #include <windows.h>
 #include <psapi.h>
 
-// TODO
+#include "staticlib/json.hpp"
+#include "staticlib/utils.hpp"
+
+#include "memlog_exception.hpp"
+
+namespace memlog {
 
 sl::json::value collect_mem_from_os() {
     PROCESS_MEMORY_COUNTERS pmc;
-    ::memset(std::addressof(pmc), '\0', sizeof (pmc));
-    auto err = ::GetProcessMemoryInfo(GetCurrentProcess(), std::addressof(pmc), sizeof (pmc));
+    ::memset(std::addressof(pmc), '\0', sizeof(pmc));
+    auto err = ::GetProcessMemoryInfo(GetCurrentProcess(), std::addressof(pmc), sizeof(pmc));
     if (0 == err) {
         throw memlog_exception(TRACEMSG("'GetProcessMemoryInfo' error: [" +
                 sl::utils::errcode_to_string(GetLastError()) + "]"));
     }
-    return static_cast<uint64_t> (pmc.WorkingSetSize);    
+    return {
+        { "overall", pmc.WorkingSetSize },
+        { "PageFaultCount", static_cast<uint64_t>(pmc.PageFaultCount) },
+        { "PeakWorkingSetSize", pmc.PeakWorkingSetSize },
+        { "WorkingSetSize", pmc.WorkingSetSize },
+        { "QuotaPeakPagedPoolUsage", pmc.QuotaPeakPagedPoolUsage },
+        { "QuotaPagedPoolUsage", pmc.QuotaPagedPoolUsage },
+        { "QuotaPeakNonPagedPoolUsage", pmc.QuotaPeakNonPagedPoolUsage },
+        { "QuotaNonPagedPoolUsage", pmc.QuotaNonPagedPoolUsage },
+        { "PagefileUsage", pmc.PagefileUsage },
+        { "PeakPagefileUsage", pmc.PeakPagefileUsage }
+    };
 }
 
+} // namespace
