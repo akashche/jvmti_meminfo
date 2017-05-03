df3e1583689c11d48c757a0e854b7d3c4120abf1
akashche <akashche@redhat.com>
2017-05-03 14:27:33 +0100

    license headers added

--- resources/plotter.js
+++ resources/plotter.js
@@ -1,5 +1,19 @@
 #!/usr/lib/jvm/java-1.8.0/bin/jjs
 
+/*
+ * Copyright 2017, akashche at redhat.com
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE.md file that
+ * accompanied this code).
+ */
+
 var files = Packages.java.nio.file.Files;
 var paths = Packages.java.nio.file.Paths;
 var system = Packages.java.lang.System;
--- src/agentmain.cpp
+++ src/agentmain.cpp
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2017, akashche at redhat.com
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE.md file that
+ * accompanied this code).
+ */
+
 /* 
  * File:   agentmain.cpp
  * Author: alex
